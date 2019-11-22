f513cc8136be0f6c139b0947eb15382ca1da4cf5
Chris Anderson <cda84097@gmail.com>
2019-11-22 07:56:19 -0700

    Finished Personal API Project

---
+++ AppleSearch/Models/AppStoreItem.swift
@@ -0,0 +1,9 @@
+//
+//  AppStoreItem.swift
+//  AppleSearch
+//
+//  Created by Chris Anderson on 11/21/19.
+//  Copyright © 2019 Renaissance Apps. All rights reserved.
+//
+
+import Foundation
---
+++ AppleSearch/Models/AppStoreItemController.swift
@@ -0,0 +1,9 @@
+//
+//  AppStoreItemController.swift
+//  AppleSearch
+//
+//  Created by Chris Anderson on 11/21/19.
+//  Copyright © 2019 Renaissance Apps. All rights reserved.
+//
+
+import Foundation
